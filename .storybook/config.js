e2b198995fcf9b64c3b1c2fdfedc622bc23575bb
Jeremias Erbs <jerry@badabam.de>
2019-11-22 14:36:30 +0100

    Add storybook addon 'info'

--- .storybook/config.js
+++ .storybook/config.js
@@ -2,11 +2,12 @@ import { configure } from '@storybook/react'
 import { load, addDecorator } from '@storybook/react'
 import styled from 'styled-components/macro'
 import React from 'react'
-
+import { withInfo } from '@storybook/addon-info'
 import GlobalStyles from '../src/GlobalStyles'
 
 // automatically import all files ending in *.stories.js
 configure(require.context('../src', true, /\.stories\.js$/), module)
+addDecorator(withInfo)
 addDecorator(storyFn => (
   <>
     <GlobalStyles />
